145ad2a4e7f03a4dfd32a63a943c37b611e49726
carlovanbaelen-wico <67262093+carlovanbaelen-wico@users.noreply.github.com>
2020-09-24 09:29:38 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,15 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (receivedNumber == 5) {
         basic.showIcon(IconNames.Happy)
     } else if (receivedNumber == 2) {
-        basic.showString("Left")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(200)
+        basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -13,7 +21,8 @@ input.onButtonPressed(Button.B, function () {
 radio.setGroup(2)
 radio.sendNumber(0)
 basic.forever(function () {
-    if (input.acceleration(Dimension.Y) < -200) {
+    led.plot(0, 0)
+    if (input.acceleration(Dimension.Y) < -500) {
         radio.sendNumber(2)
     }
 })
